dc2ff5bf430829d58dd04c64efade644f1c8187b
MD. Moshiur Rahman <rifatmoshiur00@gmail.com>
2025-11-11 19:47:53 +0600

    updating interface file

--- Typescript first day/src/interface.ts
+++ Typescript first day/src/interface.ts
@@ -1,21 +1,21 @@
-type user ={
-    name:string;
+type user = {
+    name: string;
 
-    id:number
+    id: number
 }
 
 
-type role ={
-    role : "admin" | "manager"
-} 
+type role = {
+    role: "admin" | "manager"
+}
 
 type userWithRole = user & role
 
-const mezba: user ={
-    name:   "mezba ul haque",
+const mezba: user = {
+    name: "mezba ul haque",
     id: 45
 }
-const employeeMezba : userWithRole  = {
+const employeeMezba: userWithRole = {
     role: "admin",
     name: "mizba,",
     id: 45846
@@ -24,24 +24,53 @@ const employeeMezba : userWithRole  = {
 
 // interface 
 interface IUser {
-    name: string ;
-    age : number ;
+    name: string;
+    age: number;
 }
 
 // merge with interface
 
 interface IUserWithRole extends IUser {
-     role : "admin" | "manager"
+    role: "admin" | "manager"
 }
 
 
-const jhankar:IUserWithRole ={
+const jhankar: IUserWithRole = {
     name: "jhankar",
     age: 52,
     role: "manager"
-} 
+}
 // console.log(jhankar);
+
+// example of a array to understand the interface for function
+type arr = string[]
+
+const array: arr = ['2', '3', '4']
+
+// alternate with interface
+interface IArr {
+    [index: number]: string;
+}
+const array2: IArr = ['2', '3', '4']
+
+console.log(array);
+console.log(array2);
 
 
 // function using interface 
 
+type AddFunc = (n1: number, n2: number) => number
+interface IAdd {
+    (n1: number, n2: number): number;
+}
+
+
+const add: AddFunc = (n1, n2) => n1 + n2
+
+const addd = (n1: number, n2: number): number => {
+    return n1 + n2;
+}
+
+
+console.log(add(2, 2));
+console.log(addd(2, 2));
